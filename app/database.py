c061362c5db19be2ecca00f3e73d54a2e097dbae
Nandini-m05 <nandini02102005@gmail.com>
2026-08-13 09:58:24 +0530

    Set up Alembic, create users table migration

--- app/database.py
+++ app/database.py
@@ -1,6 +1,10 @@
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+from sqlalchemy.orm import DeclarativeBase
 from app.config import settings
 
 engine = create_async_engine(settings.database_url, echo=True)
 
 async_session = async_sessionmaker(engine, expire_on_commit=False)
+
+class Base(DeclarativeBase):
+    pass
